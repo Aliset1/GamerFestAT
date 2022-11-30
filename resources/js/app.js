744c9039325e9d67fb00fae90a5a64cf7ff15e19
aaquinonez2808 <aaquinonez1@espe.edu.ec>
2022-11-29 20:35:33 -0500

    cambios

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,9 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import '../sass/app.scss';
+
+import * as bootstrap from 'bootstrap'
 
 window.Alpine = Alpine;
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,11 +10,12 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Styles -->
         @livewireStyles
+        <!-- Scripts -->
+
+
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
