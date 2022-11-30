c9eb7650d7cb5c7266b488d3ab1df095fd479449
aaquinonez2808 <aaquinonez1@espe.edu.ec>
2022-11-30 14:51:58 -0500

    avances

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,42 +17,66 @@
     </head>
     <body class="antialiased">
             <header class="header">
-                <nav class="navbar navbar-expand-lg bg-dark">
-                    <div class="container-fluid">
-                        <a class="navbar-brand" href="/"><img class="logo w-25" src="LOGO.png" alt="logoGamerFest"></a>
+                <nav class="navbar navbar-expand-lg bg-dark barra">
+                    <div class="container-fluid gap-5">
+                        <a class="navbar-brand" href="/"><img class="logo" src="LOGO.png" alt="logoGamerFest"></a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-light opcionesNav">
                             <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
                             </li>
                             <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li><hr class="dropdown-divider"></li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
+                            <a class="nav-link" href="#">Calendario</a>
                             </li>
                             <li class="nav-item">
-                            <a class="nav-link disabled">Disabled</a>
+                            <a class="nav-link" href="#">Posiciones</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">Juegos</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">Normativas</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">Noticias</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">Contactos</a>
+                            </li>
+                            <li class="nav-item">
+                            <a class="nav-link" href="#">Patrocinadores</a>
                             </li>
                         </ul>
                         <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                            <button class="btn btn-outline-success" type="submit">Search</button>
+                            <button class="btn btn-outline-success" type="submit">Iniciar Sesion</button>
                         </form>
                         </div>
                     </div>
                 </nav>
+                <div id="carouselExampleControls" class="carousel slide slider" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img src="img/slider1.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                    <img src="img/slider2.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                    <img src="..." class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+                </div>
             </header>
     </body>
 </html>
